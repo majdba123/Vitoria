2d9fee92d948fe3c8a31ffecc11812b589ef9823
Majd Bayer <137396563+majdba123@users.noreply.github.com>
2026-06-27 20:32:47 +0300

    s

--- database/migrations/2026_02_07_193127_create_products_table.php
+++ database/migrations/2026_02_07_193127_create_products_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->foreignId('vendor_id')->constrained()->cascadeOnDelete();
-            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
             $table->string('name');
             $table->text('description')->nullable();
             $table->decimal('price', 10, 2);
