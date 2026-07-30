0359098092caf5120d589648dfbd114b2a20dfaa
Majd Bayer <137396563+majdba123@users.noreply.github.com>
2026-07-30 09:57:45 +0300

    Update CityController.php

--- app/Http/Controllers/Api/CityController.php
+++ app/Http/Controllers/Api/CityController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\City;
 use Illuminate\Http\JsonResponse;
-
+//
 class CityController extends Controller
 {
     /**
